98ef5a000b2fcd5c0bbeadc2982ed97bb13e4abb
olcan <olcans@gmail.com>
2020-11-12 17:01:35 -0800

    additional formatting using vscode prettier extension

--- firebase.js
+++ firebase.js
@@ -1,25 +1,27 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyArfytkqMVwd_5TdIaKSun0iSxzjEbaRkU",
-    authDomain: "olcanswiki.firebaseapp.com",
-    databaseURL: "https://olcanswiki.firebaseio.com",
-    projectId: "olcanswiki",
-    storageBucket: "olcanswiki.appspot.com",
-    messagingSenderId: "149723699565",
-    appId: "1:149723699565:web:3027486fcea9b9b74ce264",
-    measurementId: "G-L48G2D72Q9"
-}
+  apiKey: "AIzaSyArfytkqMVwd_5TdIaKSun0iSxzjEbaRkU",
+  authDomain: "olcanswiki.firebaseapp.com",
+  databaseURL: "https://olcanswiki.firebaseio.com",
+  projectId: "olcanswiki",
+  storageBucket: "olcanswiki.appspot.com",
+  messagingSenderId: "149723699565",
+  appId: "1:149723699565:web:3027486fcea9b9b74ce264",
+  measurementId: "G-L48G2D72Q9",
+};
 
-export const isClient = typeof window !== 'undefined'
+export const isClient = typeof window !== "undefined";
 
 export function firebase() {
-    let lib = isClient ? window.firebase : require("firebase");
-    return lib.apps.length > 0 ? lib.apps[0] : lib.initializeApp(firebaseConfig)
+  let lib = isClient ? window.firebase : require("firebase");
+  return lib.apps.length > 0 ? lib.apps[0] : lib.initializeApp(firebaseConfig);
 }
 
-export function firestore() { return firebase().firestore() }
+export function firestore() {
+  return firebase().firestore();
+}
 
 export function firebaseAdmin() {
-    let admin = require("firebase-admin")
-    if (admin.apps.length == 0) admin.initializeApp(firebaseConfig)
-    return admin
+  let admin = require("firebase-admin");
+  if (admin.apps.length == 0) admin.initializeApp(firebaseConfig);
+  return admin;
 }
--- src/server.ts
+++ src/server.ts
@@ -1,32 +1,32 @@
-import sirv from 'sirv';
-import express from 'express';
-import compression from 'compression';
-import cookieParser from 'cookie-parser'
-import * as sapper from '@sapper/server';
+import sirv from "sirv";
+import express from "express";
+import compression from "compression";
+import cookieParser from "cookie-parser";
+import * as sapper from "@sapper/server";
 
 const { PORT, NODE_ENV } = process.env;
-const dev = NODE_ENV === 'development'; // NOTE: production for 'firebase serve'
+const dev = NODE_ENV === "development"; // NOTE: production for 'firebase serve'
 
-const sapperServer = express()
-	.use(
-		compression({ threshold: 0 }),
-		sirv('static', { dev }),
-		cookieParser(),
-		(req, res, next) => {
-			res.cookie = req.cookies["__session"] || ''
-			next()
-		},
-		sapper.middleware({
-			session: (req, res) => ({
-				cookie: res.cookie
-			})
-		})
-	);
+const sapperServer = express().use(
+  compression({ threshold: 0 }),
+  sirv("static", { dev }),
+  cookieParser(),
+  (req, res, next) => {
+    res.cookie = req.cookies["__session"] || "";
+    next();
+  },
+  sapper.middleware({
+    session: (req, res) => ({
+      cookie: res.cookie,
+    }),
+  })
+);
 
-if (!("FIREBASE_CONFIG" in process.env)) { // firebase handles the listening
-	sapperServer.listen(PORT).on('error', err => {
-		if (err) console.log('error', err);
-	});
+if (!("FIREBASE_CONFIG" in process.env)) {
+  // firebase handles the listening
+  sapperServer.listen(PORT).on("error", (err) => {
+    if (err) console.log("error", err);
+  });
 }
 
-export { sapperServer } // for use as handler in index.js
+export { sapperServer }; // for use as handler in index.js
