6cdd3fff6814616918eaa098dc4a3eafb1e32df2
olcan <olcans@gmail.com>
2021-02-24 11:48:34 -0800

    use lodash standard html escaping (fixes a problem due to lack of escaping for quotes), simplify welcome message

--- src/components/Item.svelte
+++ src/components/Item.svelte
@@ -7,7 +7,6 @@
   import {
     highlight,
     extractBlock,
-    unescapeHTML,
     hashCode,
     parseTags,
     renderTag,
@@ -524,7 +523,7 @@
     text = marked(text);
 
     // unwrap _html(_*) blocks
-    text = text.replace(/<pre><code class="_html_?.*?">(.*?)<\/code><\/pre>/gs, (m, _html) => unescapeHTML(_html));
+    text = text.replace(/<pre><code class="_html_?.*?">(.*?)<\/code><\/pre>/gs, (m, _html) => _.unescape(_html));
 
     // replace _math blocks, preserving whitespace
     text = text.replace(/<pre><code class="_math">(.*?)<\/code><\/pre>/gs, (m, _math) => wrapMath(_math));
@@ -576,14 +575,14 @@
     });
 
     // process any html tags with _cached attribute to effectively replace it with _cache_key="$cid"
-    text = text.replace(/<\w+.*? _cached\b.*?>/gi, function (m) {
-      if (m.match(/_cache_key/i)) {
-        console.warn("ignoring _cached attribute due to self-assigned _cache_key", name);
-        return m;
-      }
-      m = m.replace(/ _cached/, "");
-      return m.substring(0, m.length - 1) + ` _cache_key="${id}-${deephash}-${++cacheIndex}">`;
-    });
+    // text = text.replace(/<\w+.*? _cached\b.*?>/gi, function (m) {
+    //   if (m.match(/_cache_key/i)) {
+    //     console.warn("ignoring _cached attribute due to self-assigned _cache_key", name);
+    //     return m;
+    //   }
+    //   m = m.replace(/ _cached/, "");
+    //   return m.substring(0, m.length - 1) + ` _cache_key="${id}-${deephash}-${++cacheIndex}">`;
+    // });
 
     // add onclick handler to html links
     text = text.replace(/<a .*?href\s*=\s*"(.*?)".*?>/gi, function (m, href) {
