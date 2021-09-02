703f2a4702269311148c5fef934b568d37adfc6c
olcan <olcans@gmail.com>
2021-09-02 10:15:49 -0700

    allow return typeof

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -523,12 +523,12 @@
           // NOTE: we can be conservative about returns since can always be added manually
           function prependReturnIfSensible(expr) {
             // check for reserved keywords, see https://www.w3schools.com/js/js_reserved.asp
-            // NOTE: we include ALL reserved words except 'await' which obviously makes sense after return
+            // NOTE: we include ALL reserved words except await|typeof which are obviously sensible
             // NOTE: some of these keywords passed the variable name test ("let X = 0"), but we include them anyway; these were: (abstract|await|boolean|byte|char|double|final|float|goto|int|long|native|short|synchronized|throws|transient|volatile)
             // NOTE: any variable can still be returned by wrapping in parentheses, e.g. (goto)
             if (
               expr.match(
-                /^(?:\s*[})\].,:]|(?:abstract|arguments|boolean|break|byte|case|catch|char|class|const|continue|debugger|default|delete|do|double|else|enum|eval|export|extends|false|final|finally|float|for|function|goto|if|implements|import|in|instanceof|int|interface|let|long|native|new|null|package|private|protected|public|return|short|static|super|switch|synchronized|this|throw|throws|transient|true|try|typeof|var|void|volatile|while|with|yield)(?:\W|$))/
+                /^(?:\s*[})\].,:]|(?:abstract|arguments|boolean|break|byte|case|catch|char|class|const|continue|debugger|default|delete|do|double|else|enum|eval|export|extends|false|final|finally|float|for|function|goto|if|implements|import|in|instanceof|int|interface|let|long|native|new|null|package|private|protected|public|return|short|static|super|switch|synchronized|this|throw|throws|transient|true|try|var|void|volatile|while|with|yield)(?:\W|$))/
               )
             )
               return expr;
