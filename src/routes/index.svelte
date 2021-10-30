f2f8d0c425cbd3e3d700076f5f00f9fb23bf42ef
olcan <olcans@gmail.com>
2021-10-30 00:48:50 -0700

    drop the visibility requirement for modal_update, because it can be invoked before the modal actually becomes visible

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -136,7 +136,6 @@
 
   // _modal_update updates existing modal without closing it
   function _modal_update(options) {
-    if (!modal.isVisible()) throw new Error('_modal_update: modal is not visible')
     return modal.update(options)
   }
 
