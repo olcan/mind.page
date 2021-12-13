5eed0bb509359019edf6f012eae0a509661bce98
olcan <olcans@gmail.com>
2021-12-12 19:29:07 -0800

    fixed keep_empty_lines option for extractBlock, which was returning all non-matching lines as empty

--- src/util.js
+++ src/util.js
@@ -14,12 +14,20 @@ export function extractBlock(text, type, keep_empty_lines = false) {
   // NOTE: this regex is mostly consistent w/ that in updateTextDivs in Editor.svelte or toHTML in Item.svelte, and in particular allows a colon-separated prefix and suffix, w/ the suffix required to contain a period; only notable difference is that the type is allowed to match the colon-separated suffix if it matches exactly
   let insideBlock = false
   let regex = RegExp('^\\s*```(?:\\S+:)?(?:' + type + ')(?:_hidden|_removed)?(?::\\S*\\.\\S*)?(?:\\s|$)')
-  const lines = text.split('\n').map(line => {
-    if (!insideBlock && line.match(regex)) insideBlock = true
-    else if (insideBlock && line.match(/^\s*```/)) insideBlock = false
-    if (line.match(/^\s*```/)) return ''
-    return insideBlock ? line : ''
-  })
+  const lines = text
+    .split('\n')
+    .map(line => {
+      if (!insideBlock) {
+        if (line.match(regex)) insideBlock = true
+        return
+      }
+      if (line.match(/^\s*```/)) {
+        insideBlock = false
+        return '' // to get the \n after previous line
+      }
+      return line
+    })
+    .filter(l => l !== undefined)
   return keep_empty_lines
     ? lines.join('\n')
     : lines
