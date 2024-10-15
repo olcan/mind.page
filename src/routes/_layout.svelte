d6763ab7ea546e76cb3dea5c487a932db21a2501
olcan <olcans@gmail.com>
2024-10-14 20:56:34 -0700

    full npm upgrade

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,4 +1,11 @@
-<!-- these lines remove console warning: <Layout> was created with unknown prop 'segment'  -->
+<!-- this fixes "preload" is not exported error that popped up w/ newer rollup -->
+<script context="module">
+  export async function preload({ params, query }) {
+    return null
+  }
+</script>
+
+<!-- these fixes console warning: <Layout> was created with unknown prop 'segment'  -->
 <script>
   export let segment
 </script>
